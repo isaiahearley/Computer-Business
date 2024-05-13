e0c773a26e359711eb00531bcf231c6b4dcecbda
Isaiah Earley <bbsian.135@gmail.com>
2024-05-12 23:56:34 -0700

    made changes to color scheme on cookies and added toggle switches for each cookie section.
    - fixed scrolling bugs when opening and closing cookies and clicking accept, decline, and accept changes.

--- JS/cookies.js
+++ JS/cookies.js
@@ -6,35 +6,36 @@ document.addEventListener("DOMContentLoaded", function() {
     const cookieSection = document.querySelector('.cookies-section');
     const cookieManage = document.querySelector('.cookies-manage');
     const closeCookie = document.querySelector('.close-cookies-button'); 
+    const acceptChanges = document.querySelector('.cookies-manage-accept');
     const bodyFreeze = document.querySelector('body');
     const newDiv = document.createElement('div');
 
 
-
-
     acceptButton.addEventListener('click', function () {
-        console.log('Enable all cookies.');
         cookieSection.style.display = "none";
         cookieManage.style.display = "none";
 
+        //needed on all functions to prevent scroll from locking.
+        bodyFreeze.style.overflowY = 'scroll';
+        
+           
+        
     });
 
     declineButton.addEventListener('click', function () {
-        console.log('Disable all cookies.');
         cookieSection.style.display = "none";
         cookieManage.style.display = "none";
-        
+
+        bodyFreeze.style.overflowY = 'scroll';
 
     });
 
     managePreferences.addEventListener('click', function () {
-        console.log('Clicked Manage Preferences');
         cookieManage.classList.toggle('hidden');
         newDiv; 
-        newDiv.classList.toggle('cookies-dark-overlay');
+        //newDiv.classList.toggle('cookies-dark-overlay');
 
-
-
+        //needed on all elements to prevent scroll from locking.
         if (bodyFreeze.style.overflowY === 'hidden') {
             bodyFreeze.style.overflowY = 'scroll';
         } else {
@@ -46,11 +47,30 @@ document.addEventListener("DOMContentLoaded", function() {
     closeCookie.addEventListener('click', function () {
         cookieManage.classList.toggle('hidden');
 
+        //needed on all functions to prevent scroll from locking.
         if (bodyFreeze.style.overflowY === 'hidden') {
             bodyFreeze.style.overflowY = 'scroll';
         } else {
             bodyFreeze.style.overflowY = 'hidden';
         }
+    });
+
+
+
+    acceptChanges.addEventListener('click', function() {
+        console.log('Clicked on Accept Changes');
+        cookieSection.style.display = "none";
+        cookieManage.style.display = "none";
+
+        if (bodyFreeze.style.overflowY === 'hidden') {
+            bodyFreeze.style.overflowY = 'scroll';
+        } else {
+            bodyFreeze.style.overflowY = 'hidden';
+        }
+
+        //manage cookie preferences via user selections.
+
+
     });
 
 
